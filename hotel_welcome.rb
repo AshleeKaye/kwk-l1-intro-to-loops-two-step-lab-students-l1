9ff651ae4e55496d40d7eb6a962de559856df4ca
AshleeKaye <ashleemk@hotmail.com>
2018-06-06 17:54:31 +0000

    automatically backed up by learn

---
+++ hotel_welcome.rb
@@ -0,0 +1,5 @@
+def hotel_welcome(your_name, room_number)
+  puts "Welcome #{your_name.upcase} \n to the springhill suites \n Room #{room_number}"
+  
+end
+hotel_welcome("Ashlee Kaye", "333")
---
+++ loops.rb
@@ -0,0 +1,34 @@
+#loop do
+ # puts "cya later paladudes"
+#end
+
+#def dance
+ # moves_slay = 0
+  
+  #loop do
+   # if moves_slay == 10
+     # break
+   # end
+    
+   # puts "step to the left"
+    #puts "step to the right"
+   # puts "kick"
+   # moves_slay = moves_slay + 1
+ # end
+#end
+#dance
+
+def kwk_camp
+  days = 2
+  days_left = 7
+  while days < 10
+  puts "Keep coming to camp!"
+  puts "You have #{days_left} days left"
+  days = days + 1
+  days_left -= 1
+end
+puts "Camp is over :("
+end
+kwk_camp
+#A WHILE LOOP WORKS WHILE IT IS FALSE
+#AN UNTIL LOOP WORKS WHILE IT IS TRUE
